583b4c4041843ed7bfea8ac4ff5770a1ae4382fb
Ryan Elliott <ryan_m_elliott@apple.com>
2020-07-30 13:42:39 -0700

    Initial project setup

---
+++ include/ludere/Engine.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Ryan Elliott on 7/30/20.
+//
+
+#ifndef LUDERE_ENGINE_HPP
+#define LUDERE_ENGINE_HPP
+
+
+class Engine {
+
+};
+
+
+#endif //LUDERE_ENGINE_HPP
---
+++ src/core/Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ryan Elliott on 7/30/20.
+//
+
+#include "../../include/ludere/Engine.hpp"
